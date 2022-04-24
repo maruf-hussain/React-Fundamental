a73c26129fa1a46ae15cd029c91bfe06a5b07953
maruf-hussain <engmarufhussain@gmail.com>
2022-04-24 21:35:50 +0100

    second commit

--- react.js
+++ react.js
@@ -1,7 +1,9 @@
 const Phone = {
     Name: 'Samsung',
-    Price:25000
+    Price:25000,
+    Procecer: 'Sd 778'
 }
 const PhoneDetails = `
-Phone Name: ${Phone.Name}`
+Phone Name: ${Phone.Name}
+Phone Procecer: ${Phone.Procecer};`
 console.log(PhoneDetails);
